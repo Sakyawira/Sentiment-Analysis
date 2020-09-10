0262aa3e9df2b551cf47af1973f3b96d5454ca47
Sakyawira <sakyawira@gmail.com>
2020-09-10 19:32:48 +1200

    init

---
+++ keys.py
@@ -0,0 +1,5 @@
+#These keys are examples only, they won't work.
+#This is what the keys file should contain.
+client_id='MCBnOSBvG6eavg'
+client_secret='o6yyjML7L7EbapqZXOh37xQH_2I'
+username='sakyawira'
