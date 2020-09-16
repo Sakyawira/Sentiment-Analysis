0bdaf1dcf400ec27d84bba6904a268092378ac90
Sakyawira <sakyawira@gmail.com>
2020-09-17 09:51:02 +1200

    figuring out why on max of 996 posts showed up

--- api_funcs.py
+++ api_funcs.py
@@ -9,14 +9,15 @@ reddit = praw.Reddit( client_id=client_id,
                       client_secret=client_secret,
                       user_agent=f'web-app:sentimentAnalysis:v1 (by /u/{username})')
 
-def subreddit_posts(topic, limit):     
+def subreddit_posts(_topic, _limit):     
     
-    topic = reddit.subreddit('EpicGamesPC')
+    # Get the Sub-Reddit
+    topic = reddit.subreddit(_topic)
 
-    # Select the top 500 posts, with their title, URL, body, upvotes, timestamp, 
+    # Select the top <limit> posts, with their title, URL, body, upvotes, timestamp, 
     # and an index that serves as a key between the posts and the comments we collect later.
     posts = []
-    for index, post in enumerate(topic.top(limit=limit)):
+    for index, post in enumerate(topic.top(limit=_limit)):
         posts.append([post.title, "https://www.reddit.com" + post.permalink, post.selftext, post.score, post.created_utc, index])
 
     # Convert to DataFrame
@@ -32,10 +33,10 @@ def subreddit_posts(topic, limit):
 
     
 def collect_replies(key, url):
-    ''' 
-    params pandas series row: each row of the dataframe we built above in the form of a panda series
-    Returns a pandas DataFrame, where each row represents an individual comment
-    '''
+ 
+    # params pandas series row: each row of the dataframe we built above in the form of a panda series
+    # Returns a pandas DataFrame, where each row represents an individual comment
+    
     submission = reddit.submission(url=url)
     submission.comments.replace_more(limit=None)
     comment_queue = submission.comments[:] 
@@ -54,8 +55,9 @@ def collect_replies(key, url):
 
     
 def replies_to_posts(posts_dataframe):
-    ''' params pandas dataframe posts_dataframe: the posts dataframe created by the function subreddit_posts
-        returns: comments dataframe '''
+    # params pandas dataframe posts_dataframe: the posts dataframe created by the function subreddit_posts
+    # returns: comments dataframe
+    
     keys = posts_dataframe.Key.tolist()
     urls = posts_dataframe.URL.tolist()
     tupules = list(zip(keys, urls))
